745e0d845c67ecdaf6f8b910a705a70c20b4b54c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-09-08 08:18:31 +0200

    Disable linting during builds

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,9 @@ import nextBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 const withBundleAnalyzer = nextBundleAnalyzer({
