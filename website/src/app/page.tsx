75a72f3fd0ea7613a2900f907aa701c2ef229ce2
Martin Bonnin <martin@mbonnin.net>
2025-09-20 11:27:02 +0200

    add a "become a speaker" section

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -70,6 +70,7 @@ export default function GraphQLDayAtFostPage() {
       <main className="gql-all-anchors-focusable gql-conf-navbar-strip text-neu-900 before:bg-white/40 before:dark:bg-blk/30">
         <AboutEventSection />
         <div className="gql-container">
+          <BecomeASpeakerSection />
           <ExpertMeetupSection />
           <VenueAndLocationSection />
           <EventPartnersSection />
@@ -103,9 +104,11 @@ function AboutEventSection() {
         <h3 className="typography-h2 md:flex-[.5]">About</h3>
         <div className="flex flex-col gap-6 md:flex-1 text-neu-800">
           <p className="typography-body-lg">
-            Join us for a special GraphQL Day as part of the Future of Software
-            Technology, co‑located with API Days Paris. This focused event
-            brings together GraphQL practitioners, innovators, and thought
+            GraphQL day is a one day event organized by the community
+            for the community, co‑located with API Days Paris.
+          </p>
+          <p className="typography-body-lg">
+            This focused event brings together GraphQL practitioners, innovators, and thought
             leaders for a day of deep technical discussions and hands-on
             learning.
           </p>
@@ -174,7 +177,7 @@ function VenueAndLocationSection() {
             Centre of New Industries and Technologies
           </h4>
           <p className="typography-body-lg text-neu-800">
-            Located in Puteaux, commune in the western suburbs of Paris, CNIT is
+            Located in Puteaux, at the doorstep of Paris, CNIT is
             an iconic venue offering state-of-the-art facilities and easy access
             to public transportation, making it perfect for technology
             conferences.
@@ -243,6 +246,38 @@ function VenueAndLocationSection() {
         referrerPolicy="no-referrer-when-downgrade"
         className="mt-4 select-none"
       />
+    </section>
+  );
+}
+
+function BecomeASpeakerSection() {
+  return (
+    <section className="gql-section xl:py-12">
+      <h3 className="typography-h2 mb-12">Become a speaker</h3>
+      <div className="flex gap-x-12 gap-y-10 max-lg:flex-col">
+        <article className="flex flex-col gap-6 lg:flex-1">
+          <p className="typography-body-lg">
+            Any GraphQL topic is welcome: feedback from the trenches, introductions, 
+            technical deep-dives, workshop, lightning talk and more! Submit your talk 
+            using the <a href={CFP_LINK} className="typography-link">apidays typeform</a>.
+          </p>
+          <p className="typography-body-lg">
+            The apidays audience includes
+            non-GraphQL experts from other communities (AsyncAPI, OpenAPI, JSON schema, ...) as well
+            as CTOs and business deciders. Submissions that address this diverse audience are especially appreciated. You can 
+            read more in the <a href="https://www.apidays.global/speaker-guidelines" className="typography-link">apidays speaker guidelines</a>.
+          </p>
+
+          <p className="typography-body-lg">
+            The CFP closes on Oct. 12th.
+          </p>
+
+          <Button href={CFP_LINK} className="md:w-fit whitespace-nowrap">
+            Submit a proposal
+          </Button>
+
+        </article>
+      </div>
     </section>
   );
 }
