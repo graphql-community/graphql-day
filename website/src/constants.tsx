074a9844d057d68c931b8538f46fee0bfa63a387
Martin Bonnin <martin@mbonnin.net>
2025-12-11 09:14:27 +0100

    Update PLACE constant with new location details

--- website/src/constants.tsx
+++ website/src/constants.tsx
@@ -3,7 +3,7 @@ export const CFP_LINK = "https://forms.gle/2jgBp12AGBMKLSQG9";
 export const TICKETS_LINK =
   "https://ticket.apidays.global/event/apidays-paris-2025/3cccd07f-acb2-466e-8d91-cb1f208ecf42/graphql-day-at-apidays-paris-2025";
 
-export const PLACE = "CNIT La Defense, Paris";
+export const PLACE = "CNIT La Defense, Aubepine 3/4 Paris";
 
 export const ADDRESS_ELEMENT = (
   <address className="not-italic">{PLACE}</address>
